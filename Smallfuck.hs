70f2eb155335badd70a1df64f81d98f7107fab8a
Matheus Barão Fiorin <matheus.fiorin@nubank.com.br>
2023-11-06 12:15:10 -0300

    Define interpreter

--- Smallfuck.hs
+++ Smallfuck.hs
@@ -1,11 +1,18 @@
 module Smallfuck where
 
+import Data.Char (intToDigit)
+
 type Tape = [Int]
 type Pointer = Int
 type Commands = String
 
 parseTape :: String -> Tape
 parseTape = map (\c -> read [c] :: Int)
+
+interpreter :: Commands -> String -> String
+interpreter commands tape = let
+    initialTape = parseTape tape
+  in map intToDigit initialTape
 
 main :: IO()
 main = undefined
